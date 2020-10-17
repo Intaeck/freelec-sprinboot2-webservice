2a1d006b6e590f3fd5d7f35114eb8e2a6b126147
intaeck <intaeck@gmail.com>
2020-10-18 00:00:13 +0900

    bug fix @deploy.hs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl freelec-sprinboot2-webservice | grep jar | awk '{print $1}')
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [-z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
   echo "> kill -15 $CURRENT_PID"
