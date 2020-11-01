3c3bbd88180f74952f2ed09c16f9429b4faf3a98
intaeck <intaeck@gmail.com>
2020-11-01 23:22:42 +0900

    bug fix

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,7 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo "${RESPONSE}" | grep 'real' | wc -l)
+  UP_COUNT=$(echo "${RESPONSE}" | grep -c 'real' | wc -l)
   echo "> UP_COUNT :: ${UP_COUNT}"
 
   if [ "${UP_COUNT}" -ge 1 ]
